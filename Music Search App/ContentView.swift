8888926bdde2b709c84f5c6f223a1b982b81fac2
Pedro Couventaris Daspett <pedrodaspett@hotmail.com>
2022-04-05 12:03:21 -0300

    Initial Commit

---
+++ Music Search App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Search App
+//
+//  Created by Pedro Couventaris Daspett on 05/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Search App/Music_Search_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_Search_AppApp.swift
+//  Music Search App
+//
+//  Created by Pedro Couventaris Daspett on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Music_Search_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
